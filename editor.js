0b7b212352636ef384f7d8a02d716090a497a5a2
YmmSgy <briansujl@gmail.com>
2022-08-02 08:51:47 +0800

    redesigned toolbar

--- editor.js
+++ editor.js
@@ -1,41 +1,87 @@
 'use strict';
 
-const debugBar = document.getElementById('debugbar');
+const svgout = document.getElementById('svgout');
+const polygonTool = document.getElementById('polygon');
+const rectTool = document.getElementById('rect');
+const circleTool = document.getElementById('circle');
+const ellipseTool = document.getElementById('ellipse');
 
-// data of toolbar currently being moved
-const toolbarMoveData = {
-	toolbar: null,
-	pOffsetX: null,
-	pOffsetY: null
-};
-function toolbarStartDragHandler(e) {
-	toolbarMoveData.toolbar = e.currentTarget;
+let startPt = null;
+let svgoutUnitHori = 100, svgoutUnitVert = 100;
+let activeTool = null;
 
-	// get initial left and top css properties
-	const toolbarStyle = getComputedStyle(toolbarMoveData.toolbar);
-	const leftValue = toolbarStyle.left.match(/[\d\.]+(?=px)/);
-	const topValue = toolbarStyle.top.match(/[\d\.]+(?=px)/);
+function screenToSvgCd(screenCd) {
+	const svgoutRect = svgout.getBoundingClientRect();
+	const viewboxWidth = 100;
+	const viewboxHeight = 100;
+	const viewboxRatio = viewboxWidth / viewboxHeight;
 
-	// save initial pointer offset
-	toolbarMoveData.pOffsetX = e.clientX - leftValue;
-	toolbarMoveData.pOffsetY = e.clientY - topValue;
-
-	document.body.addEventListener('pointermove', toolbarDragHandler);
-	document.body.addEventListener('pointerup', toolbarEndDragHandler, {once: true});
+	if (svgoutRect.width / svgoutRect.height > viewboxRatio) {
+		const viewboxWidthInPx = svgoutRect.height * viewboxRatio;
+		const viewboxPadding = (svgoutRect.width - viewboxWidthInPx) / 2;
+		const fracViewboxWidth = (screenCd.x - viewboxPadding - svgoutRect.x) / viewboxWidthInPx;
+		return {
+			x: fracViewboxWidth * viewboxWidth,
+			y: viewboxHeight * (screenCd.y - svgoutRect.y) / svgoutRect.height
+		};
+	}
+	else {
+		const viewboxHeightInPx = svgoutRect.width / viewboxRatio;
+		const viewboxPadding = (svgoutRect.height - viewboxHeightInPx) / 2;
+		const fracViewboxHeight = (screenCd.y - viewboxPadding - svgoutRect.y) / viewboxHeightInPx;
+		return {
+			x: viewboxWidth * (screenCd.x - svgoutRect.x) / svgoutRect.width,
+			y: fracViewboxHeight * viewboxHeight
+		};
+	}
 }
-function toolbarDragHandler(e) {
-	toolbarMoveData.toolbar.classList.add('movingToolbar');
-	toolbarMoveData.toolbar.style.left = `${e.clientX - toolbarMoveData.pOffsetX}px`;
-	toolbarMoveData.toolbar.style.top = `${e.clientY - toolbarMoveData.pOffsetY}px`;
+
+function startRectMouseDown(e) {
+	startPt = screenToSvgCd({x: e.clientX, y: e.clientY});
+	addEventListener('pointermove', drawingRectMouseMove);
+	addEventListener('pointerup', endRectMouseUp, {once:true});
 }
-function toolbarEndDragHandler(e) {
-	toolbarMoveData.toolbar.classList.remove('movingToolbar');
-	document.body.removeEventListener('pointermove', toolbarDragHandler);
+function drawingRectMouseMove(e) {
+	const tmpfig = document.getElementById('__drawing');
+	if (tmpfig) {
+		// if temporary drawing figure already exists, delete figure
+		tmpfig.remove();
+	}
+	// create temporary drawing figure
+	const endPt = screenToSvgCd({x: e.clientX, y: e.clientY});
+	const outputRect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+	outputRect.setAttribute('x', Math.min(startPt.x, endPt.x));
+	outputRect.setAttribute('y', Math.min(startPt.y, endPt.y));
+	outputRect.setAttribute('width', Math.abs(endPt.x - startPt.x));
+	outputRect.setAttribute('height', Math.abs(endPt.y - startPt.y));
+	outputRect.setAttribute('id', '__drawing');
+	svgout.append(outputRect);
+}
+function endRectMouseUp(e) {
+	removeEventListener('pointermove', drawingRectMouseMove);
+	const endPt = screenToSvgCd({x: e.clientX, y: e.clientY});
+	const outputRect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+	outputRect.setAttribute('x', Math.min(startPt.x, endPt.x));
+	outputRect.setAttribute('y', Math.min(startPt.y, endPt.y));
+	outputRect.setAttribute('width', Math.abs(endPt.x - startPt.x));
+	outputRect.setAttribute('height', Math.abs(endPt.y - startPt.y));
+	svgout.append(outputRect);
+}
+function rectToolHandler(e) {
+	// toggle activeTool to/from rect
+	if (activeTool !== 'rect') {
+		activeTool = 'rect';
+		e.currentTarget.style.setProperty('background-color', 'rgb(150,150,150)');
+		svgout.addEventListener('pointerdown', startRectMouseDown);
+	}
+	else {
+		activeTool = null;
+		e.currentTarget.style.removeProperty('background-color');
+		svgout.removeEventListener('pointerdown', startRectMouseDown);
+	}
+}
+function findVH() {
+	document.documentElement.style.setProperty('--vh', `${innerHeight/100}px`);
 }
 
-// setup toolbar object to hold references to toolbars
-const toolbars = {};
-for (const toolbar of document.getElementsByClassName('toolbarCollection')) {
-	toolbar.addEventListener('pointerdown', toolbarStartDragHandler);
-	toolbars[toolbar.id] = toolbar;
-}
+rectTool.addEventListener('click', rectToolHandler);
